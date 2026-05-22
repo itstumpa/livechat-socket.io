f98c6e010317366a4ad3e0f4a08ddb8ce2931a71
itstumpa <tumpadas1fiverr@gmail.com>
2026-05-22 19:11:53 +0600

    update 6 files

--- itstumpa/backend/src/app.ts
+++ itstumpa/backend/src/app.ts
@@ -17,7 +17,7 @@ const app: Application = express();
 // middlewares
 app.use(
   cors({
-    origin: [ config.frontend_url, 'http://localhost:3000', ],
+    origin: [ config.frontend_url, "https://livechat-jade.vercel.app", 'http://localhost:3000', ],
     credentials: true,
     exposedHeaders: ["Set-Cookie"],
   }),
